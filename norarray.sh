ca8081a9f9d095a2d5639f701fb1d7f21ae941d9
Hari Prasad G <hariprasas154@gmail.com>
2022-12-22 15:28:33 +0530

    added uc8

---
+++ norarray.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+arr=()
+
+count=0;
+
+arr[((count++))]=12;
+
+echo " " ${arr[@]} ;
--- uc1.sh
+++ uc1.sh
@@ -1,34 +1,63 @@
-#!/bin/bash
+sal_hour=20;
+wrk_hour=0;
+month_sal=0;
+ttl_wrkhour=0;
 
-function calculateWorkingHour() {
-	case $1 in
-		0)
-			workingHour=0;
-		;;
-		1)
-			workingHour=8;
-		;;
-		2)
-			workingHour=4;
-		;;
-	esac;
-	echo $workingHour;
-}
+daily_sal=()
 
-totalSalary=0;
-EMP_WAGE_PER_HOUR=20;
-totalWorkingHour=0;
+
 day=1;
+count=0;
 
-while [[ $day -le 20 && $totalWorkingHour -lt 100 ]] 
+while [[ $day -le 20 && $ttl_wrkhour -lt 100 ]] 
 do
-	workHour=$(calculateWorkingHour $((RANDOM%3)));
 
-	totalWorkingHour=$(($totalWorkingHour + $workHour));
+	attendence=$((RANDOM%3));
+	case $attendence in 
+			0)
+					echo "employee is apsent ";
+					wrk_hour=0;
 	
-	salary=$(($EMP_WAGE_PER_HOUR * $workHour));
-	totalSalary=$(($totalSalary + $salary));
+			;;
+		
+			1)
+					echo "employee is present ";
+					wrk_hour=8;
+				 	
+			;;
+			2)
+					echo "employee as part time";
+
+					wrk_hour=4;
+			;;
+	
+	esac
+	ttl_wrkhour=$(($ttl_wrkhour + $wrk_hour))
+	echo " $ttl_wrkhour "
+	
+	echo "----------------"
+
+	ttl_salary=$(($sal_hour * $wrk_hour));
+
+
+	daily_sal[((count++))]=$ttl_salary
+	
+	
+	echo  " total salary of employee $ttl_salary ";
+	echo "----------------"
+	
+	month_sal=$(($month_sal + $ttl_salary));
+	
 	((day++));
+
+done
+echo "----------------"
+
+echo  " total salary of employee in month $month_sal ";
+echo "--------------------------------------------------"
+
+for (( i=1; i<=${#daily_sal[@]}; i++ ))
+do
+	echo "the sal of day $i is " ${daily_sal[$i]};
 done
 
-echo "Employee has earned $totalSalary $ in a month (Total Working Hour : $totalWorkingHour)";
