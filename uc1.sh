c339f8886acb8f381e191d3a98344675870aa480
Hari Prasad G <hariprasas154@gmail.com>
2022-12-22 13:56:32 +0530

    added

--- uc1.sh
+++ uc1.sh
@@ -1,17 +1,29 @@
 attendence=$((RANDOM%3));
-
-
+sal_hour=20;
+wrk_hour=0;
 case $attendence in 
 		0)
 				echo "employee is apsent ";
+				wrk_hour=0;
+
 		;;
 	
 		1)
-				echo "employee is present "; 
+				echo "employee is present ";
+				wrk_hour=8;
+				 
 		;;
 		2)
 				echo "employee as part time";
+
 				wrk_hour=4;
 		;;
 
 esac
+
+ttl_salary=$(($sal_hour * $wrk_hour));
+
+
+
+echo  " total salary of employee $ttl_salary ";
+
